133fbe1947ed8376d2517d96dde8a1c021ecfbff
Aktarul Islam <aktarul857@gmail.com>
2017-09-17 01:22:23 +0600

    URL Building

---
+++ tutorialspoint.com/2. URL_Building/index.js
@@ -0,0 +1,12 @@
+//Dynamic route
+var express = require('express');
+var app = express();
+
+app.get('/things/:name/:id([0-9]{5})', function(req, res){//id will be a 5 digit long number
+    res.send('ID: ' + req.params.id + '\nName: ' + req.params.name);
+});
+
+app.get('*', function(req, res){
+    res.send('Sorry, this is invalid URL. ID must be a 5 digit long number');
+});
+app.listen(8000);
